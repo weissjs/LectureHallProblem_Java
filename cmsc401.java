c5842d743c810d7c756d676eb8058d4fe2e2c5c1
Jordan Weiss <weissjs@vcu.edu>
2019-11-18 19:17:59 -0500

    commit

--- cmsc401.java
+++ cmsc401.java
@@ -3,16 +3,18 @@ import java.util.Scanner;
 public class cmsc401{
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
-    int numCourses = sc.nextInt();
+    int numCourses = sc.nextLine();
     int numRooms = sc.nextInt();
+
+    String curr_line;
 
     int[] course_arr = new int[numCourses];
 
     for(int i = 0; i < numCourses; i++){
       //course_arr[i] = sc.nextInt();
       //System.out.println("course_arr[" + i + "] = " + course_arr[i]);
-      //System.out.println(sc.next().charAt(0));
-      System.out.println((sc.nextLine()).length());
+      curr_line = sc.Line();
+      System.out.println(curr_line);
     }
   }
 }
