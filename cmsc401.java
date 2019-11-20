e135e8f3d881f5e4d501171c60531ec6b77846c2
unknown <weissjs@vcu.edu>
2019-11-19 22:33:01 -0500

    commit

--- cmsc401.java
+++ cmsc401.java
@@ -1,20 +1,63 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class cmsc401{
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
-    int numCourses = sc.nextLine();
-    int numRooms = sc.nextInt();
+    String string_Test;
+    int numCourses = Character.getNumericValue(sc.nextLine().charAt(0));
+    int numRooms = Character.getNumericValue(sc.nextLine().charAt(0));
 
-    String curr_line;
+    String[][] big_string = new String[numCourses][numRooms];
+    //System.out.println(sc.nextInt());
+    //int numCourses = sc.nextInt();
+    //int numRooms = sc.nextInt();
 
-    int[] course_arr = new int[numCourses];
-
+    // if(sc.hasNextInt()){
+    //   System.out.println("has next int");
+    // }else System.out.println("does not");
     for(int i = 0; i < numCourses; i++){
-      //course_arr[i] = sc.nextInt();
-      //System.out.println("course_arr[" + i + "] = " + course_arr[i]);
-      curr_line = sc.Line();
-      System.out.println(curr_line);
+      Scanner sc_line_by = new Scanner(sc.nextLine());
+      sc_line_by.next();
+
+      while(sc_line_by.hasNext()){
+        //string_Test = sc.next();
+        System.out.println("next is:" + sc_line_by.next());
+        //if(string_Test == "\n") break;
+      }
     }
+
+    //List<ArrayList<int>> full_list = new ArrayList<ArrayList<int>>();
+    // ArrayList<Integer>[] course = new ArrayList[numCourses];
+    //
+    // String curr_line;
+    //
+    // for (int i = 0; i < numCourses; i++) {
+    //   course[i] = new ArrayList<Integer>();
+    // }
+    // //int[] course_arr = new int[numCourses];
+    //
+    // System.out.println("numCourses :" + numCourses);
+    // System.out.println("numRooms : " +numRooms);
+    //
+    // //curr_line = sc.nextLine();
+    //
+    // //System.out.println("currline character 2: " + curr_line.charAt(1));
+    // for(int i = 0; i < numCourses; i++){
+    //   curr_line = sc.nextLine();
+    //   for(int j = 5; j < curr_line.length(); j+=3){
+    //     course[i].add(Character.getNumericValue(curr_line.charAt(j)));
+    //     //System.out.println(course[0].get(0));
+    //   }
+    // }
+    //
+    // for (int i = 0; i < numCourses; i++) {
+    //   for (int j = 0; j < course[i].size(); j++) {
+    //     System.out.print(course[i].get(j) + " ");
+    //   }
+    //   System.out.println();
+    // }
+
   }
 }
